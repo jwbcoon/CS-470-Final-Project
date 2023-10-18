7f84b686fe2774d524cbd74ed0fc2107b7a41d5f
jwbcoon <jwbcoon@gmail.com>
2023-10-17 18:30:59 -0700

    Initial project structure

---
+++ ui/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+const App = props => {
+	return (
+		<div className='layout'>
+        <p flex={4}>Hello, World!</p>
+		</div>
+	);
+}
+
+export default App;
---
+++ ui/src/SidePane.js
@@ -0,0 +1,11 @@
+import './App.css'
+
+const SidePane = props => {
+	return (
+		<div className='container'>
+            <div className='content'/>
+        </div>
+	);
+}
+
+export default SidePane;
