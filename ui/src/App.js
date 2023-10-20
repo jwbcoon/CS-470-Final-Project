80c536b76f00e1dc621c49d84d75e6410a75c907
jwbcoon <jwbcoon@gmail.com>
2023-10-20 15:03:58 -0700

    Successfully have drag and drop file uploading into the browser! Also began implementing a sidebar navigator.

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import DropZone from './components/DropZone';
+import SideNav from './components/SideNav.js';
 
 // Note: console.log(JSON.stringify(data)) will always return empty even when data is there.
 // Specify a key name like "name" within a file object and the data will present itself.
@@ -9,8 +10,11 @@ const handleFiles = data => console.log(`handling files! ${JSON.stringify(data[0
 const App = props => {
 	return (
 		<div className='layout'>
-        <p flex={4}>Hello, World!</p>
-        <DropZone className='dropzone' handleFiles={handleFiles}/>
+        <SideNav className='sidenav' style={{ backgroundColor: '#fafafa', width: '50px', height: '100%' }}/>
+        <div style={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'row' }}>
+            <p flex={4}>Hello, World!</p>
+            <DropZone className='dropzone' handleFiles={handleFiles}/>
+        </div>
 		</div>
 	);
 }
