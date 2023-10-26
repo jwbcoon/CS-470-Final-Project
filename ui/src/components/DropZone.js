7d66e7224d01f0981a510f277209734aad1681ff
jwbcoon <jwbcoon@gmail.com>
2023-10-25 22:47:41 -0700

    implemented a grid background for the EditEase viewport and refined the zoom-in/out behavior, but still struggling with the browser ctrl+wheel event overriding and defocusing the application. Changed the indentation to be more uniform across all the js files.

--- ui/src/components/DropZone.js
+++ ui/src/components/DropZone.js
@@ -2,45 +2,44 @@ import {useRef, useEffect} from 'react';
 import styles from './DropZone.module.css';
 
 function dragEnter(e) {
-  e.stopPropagation();
-  e.preventDefault();
+    e.stopPropagation();
+    e.preventDefault();
 }
 
 function dragOver(e) {
-  e.stopPropagation();
-  e.preventDefault();
+    e.stopPropagation();
+    e.preventDefault();
 }
 
 function drop(e, handleFiles, setImage) {
-  e.stopPropagation();
-  e.preventDefault();
+    e.stopPropagation();
+    e.preventDefault();
 
-  const dt = e.dataTransfer;
-  const files = dt.files;
+    const dt = e.dataTransfer;
+    const files = dt.files;
 
-  handleFiles(files, setImage);
+    handleFiles(files, setImage);
 }
 
 
 export default function DropZone(props) {
-  const ref = useRef();
+    const ref = useRef();
 
-  useEffect(() => {
-      const div = ref.current;
-      const onWheel = (e) => props.handleZoom(e, div, props.zoom, props.setZoom);
-      if (div) {
-          div.addEventListener('wheel', onWheel);
-      }
-      return () => div.removeEventListener('wheel', onWheel);
-  }, [props.zoom])
+    useEffect(() => {
+        const div = ref.current;
+        const onWheel = (e) => props.handleZoom(e, div, props.zoom, props.setZoom);
+        if (div)
+            div.addEventListener('wheel', onWheel, { passive: false });
+        return () => div.removeEventListener('wheel', onWheel);
+    }, [props.zoom])
 
-	return <div className={styles['dropzone']}
-              onDragEnter={e => dragEnter(e)}
-              onDragOver={e => dragOver(e)}
-              onDrop={e => drop(e, props.handleFiles, props.setImage)}>
-            <input type='file' style={{ display: 'none' }}/>
-            <div className={styles['mask']} ref={ref}>
-                { props.mask }
-            </div>
-         </div>;
+    return <div className={styles['dropzone']}
+                onDragEnter={e => dragEnter(e)}
+                onDragOver={e => dragOver(e)}
+                onDrop={e => drop(e, props.handleFiles, props.setImage)}>
+              <input type='file' style={{ display: 'none' }}/>
+              <div className={styles['mask']} ref={ref}>
+                  { props.mask }
+              </div>
+          </div>;
 }
--- ui/src/pages/EditEase.js
+++ ui/src/pages/EditEase.js
@@ -10,9 +10,34 @@ const handleZoom = (ev, element, zoom, setZoom) => {
     if (ev.ctrlKey) {
         console.log('in handleZoom!');
         ev.preventDefault();
-        const newZoom = ev.deltaY > 0 ? zoom + ZOOM_BASE : zoom - ZOOM_BASE;
-        element.style.transform = `scale(${newZoom})`;
-        setZoom(newZoom);
+
+        const zoomOutBoundary = 0.01, zoomInBoundary = 1000;
+        const gridSizeBound = size => size < 8 || size > 200 ? 50 : size;
+        const zoomScaler = z => {
+            if (z > 1) {
+                if (z < 10) {
+                    return ZOOM_BASE * 10;
+                }
+                else {
+                    return Math.log10(z);
+                }
+            }
+            else {
+                return ZOOM_BASE;
+            }
+        }
+
+        const viewport = document.querySelector(`.${styles['viewport']}`);
+        const zoomDelta = ev.deltaY > 0 ? zoom + zoomScaler(zoom) : zoom - zoomScaler(zoom);
+        const gridDelta = gridSizeBound(50 + (zoomDelta >= 1
+                        ? 4 * Number(zoomDelta.toPrecision(1).replace(/(\..*e\+.*)$/, ''))
+                        : -4 * Number(zoomDelta.toPrecision(1).replace(/^(0\.0*)/, ''))));
+ 
+        if (zoomDelta > zoomOutBoundary && zoomDelta < zoomInBoundary) {
+            element.style['transform'] = `scale(${zoomDelta})`;
+            viewport.style['background-size'] = `${gridDelta}px ${gridDelta}px`;
+            setZoom(zoomDelta);
+        }
     }
 }
 
@@ -25,34 +50,34 @@ const handleFiles = (data, setImage) => {
 }
 
 export default function EditEase(props) {
-  const [image, setImage] = useState({file: undefined});
-  const [zoom, setZoom] = useState(1);
+    const [image, setImage] = useState({file: undefined});
+    const [zoom, setZoom] = useState(1);
 
-  /*useEffect(() => {
-      const api = new API();
+    /*useEffect(() => {
+        const api = new API();
 
-      async function postUserOriginalImage() {
-          const postJSONResponse = await api.postUserOriginalImage(1);
-          console.log(`Response from post to database::postUserOriginalImage: ${JSON.stringify(postJSONResponse)}`);
-          //setEdit(postJSONResponse.data[0]);
-      }
+        async function postUserOriginalImage() {
+            const postJSONResponse = await api.postUserOriginalImage(1);
+            console.log(`Response from post to database::postUserOriginalImage: ${JSON.stringify(postJSONResponse)}`);
+            //setEdit(postJSONResponse.data[0]);
+        }
 
-      postUserOriginalImage();
-  }, [edit])*/
+        postUserOriginalImage();
+    }, [edit])*/
 
-	return (
-		<div className={styles['layout']}>
-        <SideNav/>
-        <div className={styles['viewport']}>
-            {
-              image.file 
-              ? <DropZone setImage={setImage} handleFiles={handleFiles} handleZoom={handleZoom}
-                          zoom={zoom} setZoom={setZoom} mask={<img src={image.file}/>}/>
-              : <DropZone setImage={setImage} handleFiles={handleFiles} handleZoom={handleZoom}
-                          zoom={zoom} setZoom={setZoom}/>
-            }
-        </div>
-		</div>
-	);
+    return (
+      <div className={styles['layout']}>
+          <SideNav/>
+          <div className={styles['viewport']}>
+              {
+                image.file 
+                ? <DropZone setImage={setImage} handleFiles={handleFiles} handleZoom={handleZoom}
+                            zoom={zoom} setZoom={setZoom} mask={<img src={image.file}/>}/>
+                : <DropZone setImage={setImage} handleFiles={handleFiles} handleZoom={handleZoom}
+                            zoom={zoom} setZoom={setZoom}/>
+              }
+          </div>
+      </div>
+    );
 }
 
