38d6cbd780455979e9c606cf09673d7085fbee47
jwbcoon <jwbcoon@gmail.com>
2023-10-30 16:08:53 -0700

    Added a page for "My Edits"

--- ui/src/pages/EditEase.js
+++ ui/src/pages/EditEase.js
@@ -2,24 +2,30 @@ import {useState, useEffect} from 'react';
 import TopNav from '../menu/TopNav';
 import ViewPort from './Viewport.js';
 import Gallery from './Gallery.js';
+import MyEdits from './MyEdits.js';
 import API from '../API_Interface/API_Interface.js';
 import styles from './EditEase.module.css';
 
-function handleOpenPage(page, setSelectedPage) {
-    setSelectedPage(page);
+function handleOpenTools() {
+    
 }
 
 export default function EditEase(props) {
     const [selectedPage, setSelectedPage] = useState({ element: <ViewPort/>, name: 'viewport' });
-    const navOptions = [{child: <p>Editor</p>,
-                         pageName: 'viewport',
-                         onClick: () => handleOpenPage({ element: <ViewPort/>, name: 'viewport' }, setSelectedPage)},
-                        {child: <p>Gallery</p>,
-                         pageName: 'gallery',
-                         onClick: () => handleOpenPage({ element: <Gallery user={props.user}/>, name: 'gallery' }, setSelectedPage)},
-                        {child: <p>My Edits</p>,
-                         pageName: 'my_edits',
-                         onClick: () => handleOpenPage(selectedPage, setSelectedPage)}];
+    const navOptions = [
+        {child: <p>Editor</p>,
+          pageName: 'viewport',
+          onClick: () => setSelectedPage({ element: <ViewPort/>, name: 'viewport' })},
+        {child: <p>Gallery</p>,
+          pageName: 'gallery',
+          onClick: () => setSelectedPage({ element: <Gallery user={props.user}/>, name: 'gallery' })},
+        {child: <p>My Edits</p>,
+          pageName: 'my_edits',
+          onClick: () => setSelectedPage({ element: <MyEdits user={props.user}/>, name: 'my_edits'})},
+        {child: <p>Open Tools</p>,
+          pageName: null,
+          onClick: () => handleOpenTools()}
+    ];
 
     return (
         <div className={styles['layout']}>
